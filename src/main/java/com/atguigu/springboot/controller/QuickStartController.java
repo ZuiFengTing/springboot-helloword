61ec028ea8e6db441b2aee90edbacb6526bdeeb0
史棋文 <shiqiwen@163.com>
2020-07-05 14:20:31 +0800

    test

---
+++ src/main/java/com/atguigu/springboot/controller/QuickStartController.java
@@ -0,0 +1,14 @@
+package com.atguigu.springboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class QuickStartController {
+
+    @RequestMapping("/quick")
+    public String quick(){
+        return "springboot 访问成功!";
+    }
+
+}
